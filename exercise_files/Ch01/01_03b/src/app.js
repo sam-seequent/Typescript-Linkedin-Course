8dbbd290dd338e59ae88186e6729dbd78afb3bc2
sam-seequent <sam.laquerre@bentley.com>
2025-06-06 10:35:27 -0600

    Ch01 complete

--- exercise_files/Ch01/01_03b/src/app.js
+++ exercise_files/Ch01/01_03b/src/app.js
@@ -1,3 +1,10 @@
+//This is an existing JS file that we will be applying TS onto based on two additional settings in the tsconfig.json file
+//Sets parameter type in the function, caused error in second call to getContact since we passed a string instead of a number
+/**
+ * 
+ * @param {number} contactId 
+ * @returns
+ */
 async function getContact(contactId) {
   const resp = await $.ajax({
     url: `/contacts/${contactId}`,
@@ -11,11 +18,13 @@ async function getContact(contactId) {
   };
 }
 
+//This function initially had errors since id is assigned a number and birthDate is assigned a Date object.
 getContact(1).then((contact) => {
-  contact.id = "1234"
-  contact.birthDate = "12/12/1990";
+  contact.id = 1234;
+  contact.birthDate = new Date("12/12/1990");
 });
 
-getContact("2").then((contact) => {
+getContact(2).then((contact) => {
   console.log("Contact: ", JSON.stringify(contact));
 });
+//Note this is how you can convert your JS code to TS code, but for the most part, you will want to write your code in TS from the start
--- exercise_files/Ch01/01_03e/src/app.js
+++ exercise_files/Ch01/01_03e/src/app.js
@@ -24,3 +24,5 @@ getContact(1).then((contact) => {
 getContact(2).then((contact) => {
   console.log("Contact: ", JSON.stringify(contact));
 });
+//There are many packages offered by the community such as JQuery, all of which can be found at https://www.npmjs.com/
+//There are thousands of packages available, and you can also create your own packages. The commands to install packages are there as well.
